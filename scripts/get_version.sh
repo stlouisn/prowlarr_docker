34629ab8ec8cb72ab4d9a8af25274b066bad15c1
stlouisn <stlouisn@hotmail.com>
2023-01-03 09:31:25 -0400

    Update get_version.sh

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Application version
-APP_VERSION="$(curl -sSL --retry 5 --retry-delay 2 "https://prowlarr.servarr.com/v1/update/develop/changes?" | jq -r '.[] | select ( .branch == "develop" ) | .version' | sort -rn | head -n 1)"
+APP_VERSION="$(curl -sSL --retry 5 --retry-delay 2 "https://prowlarr.servarr.com/v1/update/master/changes?" | jq -r '.[] | select ( .branch == "master" ) | .version' | sort -rn | head -n 1)"
 
 # Export C_VERSION
 echo "export C_VERSION=\""$APP_VERSION"\"" >> $BASH_ENV
